e41137c538a712ab382ee9faaff307be3afb55b7
WooGi1020 <jungwk1020@gmail.com>
2024-05-27 23:48:59 +0900

    ✨ feat : Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import RandomMenu from './pages/RandomMenu';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path='/'>
+        <Route index />
+        <Route path='random-menu' element={<RandomMenu />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
