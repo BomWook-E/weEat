7e518df2e22537dede7033bb49a96bc8fa520312
김보미 <kimbomi2172@naver.com>
2024-05-27 20:07:41 +0900

    ✨ feat: 카카오 API 연동

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Map } from 'react-kakao-maps-sdk';
 
 function main() {
-  return <div></div>;
+  return (
+    <div>
+      <Map
+        center={{ lat: 33.5563, lng: 126.79581 }} // 지도의 중심 좌표
+        style={{ width: '800px', height: '600px' }} // 지도 크기
+        level={3} // 지도 확대 레벨
+      ></Map>
+    </div>
+  );
 }
 
 export default main;
